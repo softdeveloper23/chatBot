4fc5b96b5c476f3282e4e6201ca85faee5bd45f3
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-17 15:35:38 +0200

    Added code to guess the age of a user

--- src/main/java/bot/SimpleBot.java
+++ src/main/java/bot/SimpleBot.java
@@ -17,5 +17,20 @@ public class SimpleBot {
         // Print user's name
         System.out.println("It's nice to meet you, " + name + "!");
 
+        // Guess user's age
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter the remainders after dividing your age by 3, 5 and 7.");
+        System.out.print("Please, enter the first remainder after dividing your age by 3: ");
+        int remainder3 = scanner.nextInt();
+        System.out.print("Please, enter the second remainder after dividing your age by 5: ");
+        int remainder5 = scanner.nextInt();
+        System.out.print("Please, enter the third remainder after dividing your age by 7: ");
+        int remainder7 = scanner.nextInt();
+        // Calculate age
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; not bad, right?");
+
+        scanner.close();
     }
 }
