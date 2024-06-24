c68b707090d809155b55021b02325a98dc8192b4
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-24 20:37:42 +0200

    Added code

--- src/main/java/bot/SimpleBot.java
+++ src/main/java/bot/SimpleBot.java
@@ -2,22 +2,36 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
+    final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // Print a greeting
-        System.out.println("Hello! My name is Simple Bot.");
-        System.out.println("I was created in 2024.");
+        // Print a greeting and request the user's name
+        greet("SimpleBot", "2024");
 
-        // Request the user's name
-        System.out.print("Please, tell me your name: ");
-
-        // Read user's name
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-
-        // Print user's name
-        System.out.println("It's nice to meet you, " + name + "!");
+        // Read and print the user's name
+        askName();
 
         // Guess user's age
+        guessAge();
+
+        // Count from 0 to n
+        count();
+
+        end();
+    }
+
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.print("Please, tell me your name: ");
+    }
+
+    static void askName() {
+        String name = scanner.nextLine();
+        System.out.println("It's nice to meet you, " + name + "!");
+    }
+
+    static void guessAge() {
         System.out.println("Let me guess your age.");
         System.out.println("Enter the remainders after dividing your age by 3, 5 and 7.");
         System.out.print("Please, enter the first remainder after dividing your age by 3: ");
@@ -28,9 +42,11 @@ public class SimpleBot {
         int remainder7 = scanner.nextInt();
         // Calculate age
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
-
+        // Print the user's age
         System.out.println(name + ", your age is " + age + "; not bad, right?");
+    }
 
+    static void count() {
         System.out.println("Now, I will prove to you that I can count to any number you want.");
 
         // Read user input and count to the number starting from 0
@@ -39,9 +55,14 @@ public class SimpleBot {
         for (int i = 0; i <= number; i++) {
             System.out.println(i + "!");
         }
+    }
 
-        System.out.println("Completed, have a nice day!");
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        // Code goes here
+    }
 
-        scanner.close();
+    static void end() {
+        System.out.println("Congratulations, have a nice day!");
     }
 }
